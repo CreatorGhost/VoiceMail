31ea43406e69eefdcc9eeedb27670470399063b5
CreatorGhost <46842909+CreatorGhost@users.noreply.github.com>
2019-03-01 00:39:35 +0530

    Update voicemail.py

--- voicemail.py
+++ voicemail.py
@@ -1,14 +1,40 @@
 import pyaudio
-import speech_recognition as sr
-r=sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say Somthing");
-    audio = r.listen(source)
-    print("Times Up...")
+
+import speech_recognition as sr  
+
+def voice():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say Somthing");
+        audio = r.listen(source)
+        print("Times Up...")
+    try:
+        text = r.recognize_google(audio)
+        print("you said : {}".format(text))
+        k = ("{}".format(text))
+        return k
+    except:
+        print("nopee")
+      
     
     
-try:
-    text = r.recognize_google(audio)
-    print("you said : {}".format(text))
-except:
-    print("fuck") 
+voice()    
+
+
+import smtplib
+
+def mymail(email,passwd,mssg):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email,passwd)
+    server.sendmail(email, "assassinsadi@gmail.com", mssg)
+    server.quit()
+
+
+if k == "send mail":
+        print("Sending Mail.....")
+        print ("Please Provide the message..")
+        message = voice()
+        mymail("wantech010@gmail.com", "Googleapj", message)
+else:
+    print("Say Send Mail")
